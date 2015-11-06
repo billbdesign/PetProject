3a2ab2e757c4be80280db6002782e8a2f193444d
billbdesign <billbdesign@gmail.com>
2015-11-06 12:11:38 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('document').ready(function() {
+	var options = { videoId: 'Ghqseip2D-k', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+	// fZ2wVRFl_MU live cats
+	// fIlJyZEbrzo ocean loop
+});
